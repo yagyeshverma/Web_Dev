d42480b0a2bd3a9e6a2274e15c9d2128a6cadd80
D Verma <verma453h@gmail.com>
2023-10-24 17:29:49 +0530

    Web_Dev

---
+++ Frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+           Created By YAGYESH &rarr; 2023-2024| &copy; All Rights Reserved..
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
